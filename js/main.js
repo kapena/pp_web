743379f49dcfc8f27c68ad5d220b29fa66103728
Brent <brentw.white@gmail.com>
2014-12-15 22:12:47 -1000

    services-icons/hover ham-nav/links added

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function hover(element) {
+	element.setAttribute('src', 'http://d3vi9nkvdbmq5l.cloudfront.net/service-icons/hov/Repair_h.svg');
+}
+
+function unhover(element) {
+	element.setAttribute('src', 'http://d3vi9nkvdbmq5l.cloudfront.net/service-icons/Repair.svg');
+}
