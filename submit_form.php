eb9c9880814e7d3a1ef8ca3930cfc8c804d1d98d
Brent <brentw.white@gmail.com>
2014-12-27 00:27:57 -1000

    test-3 mail no var in php

--- submit_form.php
+++ submit_form.php
@@ -9,7 +9,7 @@ if($_POST["submit"]) {
 	$message = $_POST["message-me"];
 
 $mailBody="Name: $name\nEmail: $email\n\n$message";
-$mail($recipient, $subject, $mailBody, "From: $name <$email>");
+mail($recipient, $subject, $mailBody, "From: $name <$email>");
 $thankYou="<p>Thank you! We will be in contact with you shortly.</p>";
 }
 ?>
