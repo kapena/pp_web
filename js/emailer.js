d66ac752c9c2917641095a1b5919270c28a5370d
Brent <brentw.white@gmail.com>
2014-12-29 17:05:51 -1000

    error 400

--- js/emailer.js
+++ js/emailer.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
  
   // Initialize Parse with your Parse application & javascript keys
-  Parse.initialize("cMvXwglt3OcBRoFNl67SwEmyihzlARPypRQGYhx9", "oYGbrWnG9rGl65q9sz3LAqVLOZNIdZuAJTv1m3tS");
+  Parse.initialize("app-key", "parse-js-key");
  
   // Setup the form to watch for the submit event
   $('.pp-form').submit(function(e){
